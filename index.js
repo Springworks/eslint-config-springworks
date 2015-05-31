a7ee82fd749eaad08955e37d60cb33c6254c3d2e
David Brockman Smoliansky <david@springworks.se>
2015-06-01 00:43:07 +0200

    Warn if no empty lines before comments

--- index.js
+++ index.js
@@ -375,6 +375,15 @@ module.exports = {
       }
     ],
 
+    // enforces empty lines around comments (off by default)
+    "lines-around-comment": [
+      1,
+      {
+        "beforeBlockComment": true,
+        "beforeLineComment": true
+      }
+    ],
+
     // disallow mixed "LF" and "CRLF" as linebreaks (off by default)
     "linebreak-style": [2, "unix"],
 

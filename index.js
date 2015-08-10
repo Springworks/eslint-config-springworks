ab7540099aab0555d79bd77bf88a166f34829501
David Brockman Smoliansky <david@springworks.se>
2015-08-10 20:09:04 +0200

    Change callback-return to warn
    Lower the lever to "warn" since a lot of callbacks are invoked in if/else statements.

--- index.js
+++ index.js
@@ -329,7 +329,7 @@ module.exports = {
      */
 
     // enforce return after a callback
-    "callback-return": [2, ["callback", "cb", "next", "done"]],
+    "callback-return": [1, ["callback", "cb", "next", "done"]],
 
     // enforces error handling in callbacks (on by default in the node environment)
     "handle-callback-err": [2, "^(.+_)?err(or)?$"],

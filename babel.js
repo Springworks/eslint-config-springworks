fcf38c57e4a706a9db852889f72c206e45b3d6b2
David Brockman Smoliansky <david@springworks.se>
2015-08-24 18:30:13 +0200

    Enable the new ES6 rules introduced in 1.2.0

--- babel.js
+++ babel.js
@@ -35,6 +35,9 @@ module.exports = {
     // disallow modifying variables that are declared using `const`
     "no-const-assign": 2,
 
+    // disallow duplicate name in class members
+    "no-dupe-class-members": 2,
+
     // disallow to use this/super before super() calling in constructors.
     "no-this-before-super": 2,
 
@@ -44,6 +47,9 @@ module.exports = {
     // require method and property shorthand syntax for object literals
     "object-shorthand": [1, "always"],
 
+    // suggest using arrow functions as callbacks
+    "prefer-arrow-callback": 1,
+
     // suggest using of const declaration for variables that are never modified after declared
     "prefer-const": 1,
 
@@ -52,6 +58,9 @@ module.exports = {
 
     // suggest using Reflect methods where applicable
     "prefer-reflect": 0,
+
+    // suggest using template literals instead of strings concatenation
+    "prefer-template": 1,
 
     // disallow generator functions that do not have `yield`
     "require-yield": 2

8b766cd7db6a7fdf51feaa04c8c6e3fc790b2dc6
Magnus Wolffelt <magnus.wolffelt@gmail.com>
2015-09-16 18:45:41 +0200

    Add example.js, npm scripts and make local lint work by splitting into js and json files

--- mocha.js
+++ mocha.js
@@ -1,31 +1,4 @@
-module.exports = {
-  "env": {
-    "mocha": true
-  },
-
-  "plugins": [
-    "mocha"
-  ],
-
-  "rules": {
-
-    // Override to allow asserting that a function throws: (function() { fn(); }).should.throw();
-    "no-wrap-func": 0,
-
-    // Override to allow use of synchronous functions in tests. Useful when loading fixtures for example.
-    "no-sync": 0,
-
-    "max-nested-callbacks": 0,
-
-    "padded-blocks": 0,
-
-    "no-extra-parens": 0,
-
-    // Enforces handling of callbacks for async tests.
-    "mocha/handle-done-callback": 2,
-
-    // Reminds you to remove .only from your tests.
-    "mocha/no-exclusive-tests": 2
-
-  }
-};
+/** This js file is only here to make shareable config work
+    http://eslint.org/docs/developer-guide/shareable-configs.html
+ */
+module.exports = require("mocha-config.json");

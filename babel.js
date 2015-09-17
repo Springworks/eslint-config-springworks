65c44ca87565ce320d952a89a2620ca3293765cf
Magnus Wolffelt <magnus.wolffelt@gmail.com>
2015-09-17 11:49:33 +0200

    Add ./ on config js files to make the goodness work

--- babel.js
+++ babel.js
@@ -1,4 +1,4 @@
 /** This js file is only here to make shareable config work
     http://eslint.org/docs/developer-guide/shareable-configs.html
  */
-module.exports = require("babel-config.json");
+module.exports = require("./babel-config.json");
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 /** This js file is only here to make shareable config work
     http://eslint.org/docs/developer-guide/shareable-configs.html
  */
-module.exports = require('index-config.json');
+module.exports = require('./index-config.json');

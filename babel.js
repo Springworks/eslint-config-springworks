1dbb6b0fa95cfb43579a70d40cd32b964fa710b4
David Brockman Smoliansky <david@springworks.se>
2016-05-03 21:02:33 +0200

    fix: move linter config to js files
    There is no need to have the split into a js and a json file anymore.

--- babel.js
+++ babel.js
@@ -1,4 +1,22 @@
-/** This js file is only here to make shareable config work
-    http://eslint.org/docs/developer-guide/shareable-configs.html
- */
-module.exports = require("./babel-config.json");
+module.exports = {
+  "extends": "./index.js",
+  "parser": "babel-eslint",
+  "plugins": [
+    "import"
+  ],
+  "rules": {
+    "import/default": 2,
+    "import/export": 2,
+    "import/imports-first": 2,
+    "import/named": 2,
+    "import/namespace": 2,
+    "import/no-duplicates": 0,
+    "import/no-named-as-default": 2,
+    "import/no-require": 0,
+    "import/no-unresolved": 2,
+
+    "no-duplicate-imports": 2,
+    "prefer-spread": 2,
+    "strict": [1, "never"]
+  }
+};
